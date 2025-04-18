3d6c1fcada496ffbf1a35092091dc7d999afddaf
MKdivine <jensboerger23@gmail.com>
2025-04-18 22:39:52 +0200

    start of the project everything works

---
+++ app.js
@@ -0,0 +1,7 @@
+// Calculator functions
+
+const addNumbers = (a, b) => a + b;
+const subtractNumbers = (a, b) => a - b;
+const multiplyNumbers = (a, b) => a * b;
+const divideNumbers = (a, b) => a / b;
+// all functions work in console
