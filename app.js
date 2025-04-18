6f01444187933e23227f472e804c5f076f7915b9
MKdivine <jensboerger23@gmail.com>
2025-04-18 23:18:33 +0200

    start of the project everything works

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ const subtractNumbers = (a, b) => a - b;
 const multiplyNumbers = (a, b) => a * b;
 const divideNumbers = (a, b) => a / b;
 // all functions work in console
+
+// calculator logic
+
+let number1 = 2;
+let number2 = 2;
+let operator = "+";
+
+const operate = (a, b, operator) => {
+    switch (operator) {
+        case "+":
+            return addNumbers(a, b);
+        case "-":
+            return subtractNumbers(a, b);
+        case "*":
+            return multiplyNumbers(a, b);
+        case "/":
+            return divideNumbers(a, b);
+    }
+}
+const calcResult = operate(number1, number2, operator);
+
+// Calculator DOM UI
+const resultDisplay = document.getElementById("result");
+
+resultDisplay.textContent = calcResult;
+const display = result.appendChild(calcResult);
+
+
+
+
+
